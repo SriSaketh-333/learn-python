274cb3ac657f66e6d4431f840325cf7187387dcb
vicky@123 <73951393+vicky150612@users.noreply.github.com>
2021-04-24 12:53:38 +0530

    repeted

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,17 @@
 print("Welcome to the calculator")
-fir = int(input("Type your first number"))
-sec = int(input("Type your second number"))
-x = input("Type any operation name").lower()
-if x == "addition":
-   print("The answer is " + str(fir + sec))
-elif x == "subtraction":
-   print("The answer is " + str(fir - sec))
-elif x == "multiplication":
-   print("The answer is " + str(fir * sec))
-elif x == "division":
-   print("The answer is " + str(fir / sec))
-else:
-    print("check your spelling")
+q = input('claculator or quit')
+while q != 'q','quit'
+   fir = int(input("Type your first number"))
+   sec = int(input("Type your second number"))
+   x = input("Type any operation name").lower()
+   if x == "addition":
+      print("The answer is " + str(fir + sec))
+   elif x == "subtraction":
+      print("The answer is " + str(fir - sec))
+   elif x == "multiplication":
+      print("The answer is " + str(fir * sec))
+   elif x == "division":
+      print("The answer is " + str(fir / sec))
+   else:
+       print("check your spelling")
 print("Thank you")
